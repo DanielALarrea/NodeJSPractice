50a9120bab97a2961ff71cc8c22683c57eeeff47
Daniel Larrea <larread6286@my.uwstout.edu>
2020-08-12 14:57:56 -0500

    More NodeJS practice. Some practice with Express

---
+++ timeout.js
@@ -0,0 +1,12 @@
+function printHello() {
+    console.log("Hello world!");
+}
+
+// Display after some time
+// var t = setTimeout(printHello, 2000);
+
+// Dont display after some time
+// clearTimeout(t);
+
+// Print every so often
+setInterval(printHello, 2000);
